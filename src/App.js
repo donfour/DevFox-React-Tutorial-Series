f5ebab5dbefb7350af721c2f6d76b91a36740f4f
donfour <donovanso@yahoo.com.hk>
2018-06-06 21:17:49 +0800

    added section 9

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import Blog from './Blog';
+import Post from './Post';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      value: "",
-      valid: false
-    }
-    this.processInput = this.processInput.bind(this)
-  }
-  processInput(){
-    console.log(this.state.value);
-  }
-
   render() {
     return (
       <div>
-        {this.state.valid ? "Valid Password!" : "Invalid Password!"}
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={(e)=>{
-              const value = e.target.value;
-              let valid;
-              if(value.length >= 8 && value.replace(/[^A-Z]/g, "").length >= 1){
-                valid = true;
-              } else {
-                valid = false;
-              }
-              this.setState({
-                value,
-                valid
-              })
-            }}
-          />
-        <button onClick={this.processInput}>Submit</button>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/blog'>Blog</Link></li>
+        </ul>
+          <Switch>
+            <Route exact path='/blog' component={Blog}/>
+            <Route path='/blog/:postid' component={Post}/>
+          </Switch>
       </div>
-    );
+    )
   }
 }
 
