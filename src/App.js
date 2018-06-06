bcfc4812e838e2d7f74f77a8ededbac054ecdd0b
donfour <donovanso@yahoo.com.hk>
2018-06-06 21:12:53 +0800

    added section 6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,86 @@
 import React, { Component } from 'react';
+import Square from './Square';
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      count: 0
+  state = {
+    board: ["", "", "",
+            "", "", "",
+            "", "", ""],
+    turn: "X"
+  }
+  updateBoard(i){
+    const board = this.state.board.slice();
+    const turn = this.state.turn;
+    if(board[i]===""){
+      board[i] = turn;
     }
-    this.incrementCount = this.incrementCount.bind(this);
-    this.decrementCount = this.decrementCount.bind(this);
+    this.setState({
+      board,
+      turn: turn === "O" ? "X" : "O"
+    })
   }
 
-  incrementCount(){
-    this.setState((prevState) => {
-      return {count: prevState.count + 1}
-    });
+  playerWon() {
+    const board = this.state.board;
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    // determining if someone won
+    for(let i = 0; i < winningConditions.length; i++){
+      const [a, b, c] = winningConditions[i];
+      if(board[a] !== "" &&
+         board[a] === board[b] &&
+         board[b] === board[c])
+      {
+        return board[winningConditions[i][0]]
+      }
+    }
+    // check if board is full
+    let isBoardFull = true;
+    for(let i = 0; i < board.length; i++){
+      if( board[i] === "" ){
+        isBoardFull = false;
+      }
+    }
+    if(!isBoardFull){
+      // if no one won and the board is not full, game continues
+      return ""
+    } else {
+      // if no one won when the board is full, it is a tie
+      return "tie";
+    }
   }
-
-  decrementCount(){
-    this.setState((prevState) => {
-      return {count: prevState.count - 1}
-    });
-  }
-
   render() {
+    const board = this.state.board;
+
     return (
       <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.incrementCount}>+</button>
-        <button onClick={this.decrementCount}>-</button>
+        <h1>{this.playerWon()}</h1>
+        <div>
+          <Square value={board[0]} handleClick={this.updateBoard.bind(this, 0)}/>
+          <Square value={board[1]} handleClick={this.updateBoard.bind(this, 1)}/>
+          <Square value={board[2]} handleClick={this.updateBoard.bind(this, 2)}/>
+        </div>
+        <div>
+          <Square value={board[3]} handleClick={this.updateBoard.bind(this, 3)}/>
+          <Square value={board[4]} handleClick={this.updateBoard.bind(this, 4)}/>
+          <Square value={board[5]} handleClick={this.updateBoard.bind(this, 5)}/>
+        </div>
+        <div>
+          <Square value={board[6]} handleClick={this.updateBoard.bind(this, 6)}/>
+          <Square value={board[7]} handleClick={this.updateBoard.bind(this, 7)}/>
+          <Square value={board[8]} handleClick={this.updateBoard.bind(this, 8)}/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
