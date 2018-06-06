3259b61f6286dad9caa8735d9fd92420685e0fe8
donfour <donovanso@yahoo.com.hk>
2018-06-06 21:10:19 +0800

    added section 5

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import GroceryList from './GroceryList';
-
-const mondayList = [{
-                     key: "1",
-                     name: "Bread"
-                    },
-                    {
-                      key: "2",
-                      name: "Peanut Butter"
-                    },
-                    {
-                      key: "3",
-                      name: "Jelly"
-                    }]
-
-const tuesdayList = [{
-                      key: "4",
-                      name: "Cereal"
-                     },
-                     {
-                      key: "5",
-                      name: "Cup noodles"
-                     }]
-
-const wednesdayList = [{
-                       key: "6",
-                       name: "Beer"
-                       }]
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.incrementCount = this.incrementCount.bind(this);
+    this.decrementCount = this.decrementCount.bind(this);
+  }
+
+  incrementCount(){
+    this.setState((prevState) => {
+      return {count: prevState.count + 1}
+    });
+  }
+
+  decrementCount(){
+    this.setState((prevState) => {
+      return {count: prevState.count - 1}
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Header />
-        <GroceryList dayOfWeek={"Monday"} data={mondayList} />
-        <GroceryList dayOfWeek={"Tuesday"} data={tuesdayList} />
-        <GroceryList dayOfWeek={"Wednesday"} data={wednesdayList} />
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.incrementCount}>+</button>
+        <button onClick={this.decrementCount}>-</button>
       </div>
     );
   }
