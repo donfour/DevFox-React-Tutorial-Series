2a252ef168d84a3127ba3265e88c6b358110b8a0
donfour <donovanso@yahoo.com.hk>
2018-06-06 21:09:13 +0800

    added section 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import Header from './Header';
 import GroceryList from './GroceryList';
 
+const mondayList = [{
+                     key: "1",
+                     name: "Bread"
+                    },
+                    {
+                      key: "2",
+                      name: "Peanut Butter"
+                    },
+                    {
+                      key: "3",
+                      name: "Jelly"
+                    }]
+
+const tuesdayList = [{
+                      key: "4",
+                      name: "Cereal"
+                     },
+                     {
+                      key: "5",
+                      name: "Cup noodles"
+                     }]
+
+const wednesdayList = [{
+                       key: "6",
+                       name: "Beer"
+                       }]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <GroceryList />
+        <GroceryList dayOfWeek={"Monday"} data={mondayList} />
+        <GroceryList dayOfWeek={"Tuesday"} data={tuesdayList} />
+        <GroceryList dayOfWeek={"Wednesday"} data={wednesdayList} />
       </div>
     );
   }
--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class GroceryList extends Component {
   render(){
     return(
-      <ol>
-        <li>Bread</li>
-        <li>Peanut butter</li>
-        <li>Jelly</li>
-      </ol>
+      <div>
+        <h3>{this.props.dayOfWeek}</h3>
+        <ol>
+          {this.props.data.map((item)=><li key={item.key}>{item.name}</li>)}
+        </ol>
+      </div>
     )
   }
 }
+
+GroceryList.defaultProps = {
+  data:[]
+}
+
+GroceryList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string,
+    name: PropTypes.string
+  })),
+};
