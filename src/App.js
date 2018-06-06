f1671eac093c13070931aca3681cfbc0d0467430
donfour <donovanso@yahoo.com.hk>
2018-06-06 21:01:45 +0800

    added master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import GroceryList from './GroceryList';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <Header />
-        <GroceryList />
-      </div>
+      <div></div>
     );
   }
+
 }
 
 export default App;
